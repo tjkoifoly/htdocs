7f5bb615380ccf24aed2c9f84ebbd750b3510a9b
tjkoifoly <foly.v1.8.01@gmail.com>
2013-05-03 17:20:22 +0700

    cmm

--- PHPUpload/index.php
+++ PHPUpload/index.php
@@ -7,6 +7,15 @@
             <input type="file" name="file" id="file"><br>
             <input type="submit" name="submit" value="Submit">
         </form>
+        <?php
+        $query = $_SERVER['PHP_SELF'];
+        $path = pathinfo($query);
+        $what_you_want = $path['dirname'];
+        
+        print_r($path);
+        echo "HOST = " . $what_you_want;
+        
+        ?>
 
     </body>
 </html>
--- PHPUpload/upload_file.php
+++ PHPUpload/upload_file.php
@@ -3,19 +3,18 @@
 $dir = "upload/";
 if (!file_exists($dir) && !is_dir($dir)) {
     echo "Pass here - check<br/>";
-    mkdir($dir);         
+    mkdir($dir);
     chmod($dir, 0777);
-} else if(file_exists($dir) && is_dir($dir)){
+} else if (file_exists($dir) && is_dir($dir)) {
     echo "Pass here - check 2k<br/>";
-   chmod($dir, 0777);
+    chmod($dir, 0777);
 }
 
 $allowedExts = array("gif", "jpeg", "jpg", "png");
 $extension = end(explode(".", $_FILES["file"]["name"]));
 
-echo "File type is : " .$_FILES["file"]["type"];
-if($_FILES["file"]["type"] == "application/zip")
-{
+echo "File type is : " . $_FILES["file"]["type"];
+if ($_FILES["file"]["type"] == "application/zip") {
     
 }
 
@@ -28,10 +27,9 @@ if ((($_FILES["file"]["type"] == "image/gif")
         && ($_FILES["file"]["size"] < 2000000)
         && in_array($extension, $allowedExts)) {
     echo "Pass here ---> 1k<br/>";
-    
+
     if ($_FILES["file"]["error"] > 0) {
         echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-        
     } else {
         echo "Upload: " . $_FILES["file"]["name"] . "<br>";
         echo "Type: " . $_FILES["file"]["type"] . "<br>";
@@ -41,11 +39,27 @@ if ((($_FILES["file"]["type"] == "image/gif")
         if (file_exists("upload/" . $_FILES["file"]["name"])) {
             echo $_FILES["file"]["name"] . " already exists. ";
         } else {
+            $des_dir = "upload/" . $_FILES["file"]["name"];
             move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-            echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+            echo "Stored in: " . $des_dir. "<br />";
+
+            $query = $_SERVER['PHP_SELF'];
+            $path = pathinfo($query);
+            $what_you_want = $path['dirname'];
+
+            print_r($path);
+            $link = $_SERVER['HTTP_HOST'].$what_you_want;
+            $protocol = "http://";
+            if(isset($_SERVER['HTTPS']))
+            {
+                $protocol = "https://";
+            }
+                
+            echo "<br />$protocol$link/$des_dir<br />";
+            
+            echo "<img src='$protocol$link/$des_dir' />";
         }
     }
-    
 } else {
     echo "Invalid file";
 }
