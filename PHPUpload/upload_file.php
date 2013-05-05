846bab5f9d323664a0b580c660c56eaf01ff2df3
tjkoifoly <foly.v1.8.01@gmail.com>
2013-05-05 11:21:28 +0700

    new prj

--- PHPUpload/upload_file.php
+++ PHPUpload/upload_file.php
@@ -11,36 +11,36 @@ if (!file_exists($dir) && !is_dir($dir)) {
 }
 
 $allowedExts = array("gif", "jpeg", "jpg", "png");
-$extension = end(explode(".", $_FILES["file"]["name"]));
+$extension = end(explode(".", $_FILES["avatar"]["name"]));
 
-echo "File type is : " . $_FILES["file"]["type"];
-if ($_FILES["file"]["type"] == "application/zip") {
-    
+echo "File type is : " . $_FILES["avatar"]["type"];
+if ($_FILES["avatar"]["type"] == "application/zip") {
+    echo "ZIP file has been selected";
 }
 
-if ((($_FILES["file"]["type"] == "image/gif")
-        || ($_FILES["file"]["type"] == "image/jpeg")
-        || ($_FILES["file"]["type"] == "image/jpg")
-        || ($_FILES["file"]["type"] == "image/pjpeg")
-        || ($_FILES["file"]["type"] == "image/x-png")
-        || ($_FILES["file"]["type"] == "image/png"))
-        && ($_FILES["file"]["size"] < 2000000)
+if ((($_FILES["avatar"]["type"] == "image/gif")
+        || ($_FILES["avatar"]["type"] == "image/jpeg")
+        || ($_FILES["avatar"]["type"] == "image/jpg")
+        || ($_FILES["avatar"]["type"] == "image/pjpeg")
+        || ($_FILES["avatar"]["type"] == "image/x-png")
+        || ($_FILES["avatar"]["type"] == "image/png"))
+        && ($_FILES["avatar"]["size"] < 2000000)
         && in_array($extension, $allowedExts)) {
     echo "Pass here ---> 1k<br/>";
 
-    if ($_FILES["file"]["error"] > 0) {
-        echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    if ($_FILES["avatar"]["error"] > 0) {
+        echo "Return Code: " . $_FILES["avatar"]["error"] . "<br>";
     } else {
-        echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-        echo "Type: " . $_FILES["file"]["type"] . "<br>";
-        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-        echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+        echo "Upload: " . $_FILES["avatar"]["name"] . "<br>";
+        echo "Type: " . $_FILES["avatar"]["type"] . "<br>";
+        echo "Size: " . ($_FILES["avatar"]["size"] / 1024) . " kB<br>";
+        echo "Temp file: " . $_FILES["avatar"]["tmp_name"] . "<br>";
 
-        if (file_exists("upload/" . $_FILES["file"]["name"])) {
-            echo $_FILES["file"]["name"] . " already exists. ";
+        if (file_exists("upload/" . $_FILES["avatar"]["name"])) {
+            echo $_FILES["avatar"]["name"] . " already exists. ";
         } else {
-            $des_dir = "upload/" . $_FILES["file"]["name"];
-            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
+            $des_dir = "upload/" . $_FILES["avatar"]["name"];
+            move_uploaded_file($_FILES["avatar"]["tmp_name"], "upload/" . $_FILES["avatar"]["name"]);
             echo "Stored in: " . $des_dir. "<br />";
 
             $query = $_SERVER['PHP_SELF'];
--- gift-love/index.php
+++ gift-love/index.php
@@ -7,7 +7,6 @@
     <body>
         <?php
         // put your code here
-        echo 'Hello World';
         ?>
     </body>
 </html>
