db984bc12601bacf16f33a33cf93b320b3b30b44
tjkoifoly <foly.v1.8.01@gmail.com>
2013-05-13 15:42:58 +0700

    fix

--- gift-love/list_friend.php
+++ gift-love/list_friend.php
@@ -7,7 +7,7 @@ $sourceID = $_POST['sourceID'];
 
 function get_friend_list($sID) {
     if (connect_databse()) {
-        $query = "SELECT account.*, relationship.rsStatus From relationship LEFT JOIN account ON rsFriendID = accID WHERE accID IN(SELECT DISTINCT rsFriendID FROM account JOIN relationship ON account.accID = relationship.rsSourceID AND accID = $sID)  LIMIT 100;
+        $query = "SELECT DISTINCT account.*, relationship.rsStatus From relationship LEFT JOIN account ON rsFriendID = accID WHERE accID IN(SELECT DISTINCT rsFriendID FROM account JOIN relationship ON account.accID = relationship.rsSourceID AND accID = $sID)  LIMIT 100;
 ";
         $result = mysql_query($query) or die(mysql_error());
         $num = mysql_num_rows($result);
